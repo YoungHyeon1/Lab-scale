ee9d4b7f37b1207dde3a3f701ebcd643600a046a
YoungHyeon1 <73692488+YoungHyeon1@users.noreply.github.com>
2024-06-05 22:33:45 +0900

    temp

--- Backend/utils/s3_to_rdb.py
+++ Backend/utils/s3_to_rdb.py
@@ -1,2 +1,11 @@
+import pandas as pd
+
+matches_data = pd.read_csv('s3://mybucket/myfile.csv')
+
+# pandas to sql
+
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo=False)
+matches_data.to_sql('matches', con=engine)
 
 
