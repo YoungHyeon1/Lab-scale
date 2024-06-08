a03e817ffffc1f688ac2646b80aa5cca0c82eb85
YoungHyeon1 <73692488+YoungHyeon1@users.noreply.github.com>
2024-06-08 14:47:52 +0900

    fix rdb storage & parsing db

--- Backend/utils/s3_to_rdb.py
+++ Backend/utils/s3_to_rdb.py
@@ -34,7 +34,9 @@ def stream_s3_file(bucket, key):
     S3에서 파일을 스트리밍하고 각 줄을 처리하는 함수.
     """
     response = s3.get_object(Bucket=bucket, Key=key)
-    for line in response['Body'].iter_lines():
+    for index, line in enumerate(response['Body'].iter_lines()):
+        if index <= 489243:
+            continue
         line = line.decode('utf-8')  # 바이트를 문자열로 디코드
         result = process_line(line)
         if result is not None:
@@ -50,7 +52,7 @@ utc_zone = pytz.utc
 matches_id = {int(row.match_id) for row in session.query(Matches.match_id).all()}
 matches_id = set(matches_id)
 
-for processed_data in stream_s3_file(bucket, key):
+for index, processed_data in enumerate(stream_s3_file(bucket, key)):
     # 처리된 데이터를 사용하여 여기에서 추가 작업을 수행
     if processed_data["info"]["gameId"] in matches_id:
         print("Already exist")
@@ -59,7 +61,10 @@ for processed_data in stream_s3_file(bucket, key):
     user_list = list()
     for user_info in processed_data["info"]["participants"]:
         # overlab check
-        user = session.query(Users).filter_by(puuid=user_info['puuid']).one_or_none()
+        if user_info['puuid'] == 'BOT':
+            user = session.query(Users).filter_by(puuid='BOT').one_or_none()
+        else:
+            user = session.query(Users).filter_by(puuid=user_info['puuid']).one_or_none()
         if user is None:
             user = (Users(
                 puuid=user_info["puuid"],
@@ -89,4 +94,5 @@ for processed_data in stream_s3_file(bucket, key):
         session.rollback()
     except Exception as e:
         print(e)
-session.close()
+    finally:
+        session.close()
