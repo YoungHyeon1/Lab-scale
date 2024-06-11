8e17e200887fcd193ebfa268b286d3acc8ff5b0c
YoungHyeon1 <73692488+YoungHyeon1@users.noreply.github.com>
2024-06-11 10:53:51 +0900

    varlables

--- Infra/variables.tf
+++ Infra/variables.tf
@@ -28,17 +28,17 @@ variable "account_id" {
 
 variable "db_subnet_id" {
   description = "The ID of the subnet in the availability zone 2b"
-  type       = list(string)
+  type        = list(string)
 }
 
 variable "riot_db_username" {
   description = "riot DB username"
-  type       = string
+  type        = string
 }
 
 variable "riot_db_password" {
   description = "roptr_db_password"
-  type       = string
+  type        = string
 }
 
 variable "subnet_id_2b" {
@@ -49,11 +49,11 @@ variable "subnet_id_2b" {
 
 variable "cidr_private_subnet" {
   description = "CIDR block for the private subnet"
-  default     = "172.31.0.0/16" # 예시 CIDR, 필요에 따라 변경
+  default     = "172.31.32.0/24" # 예시 CIDR, 필요에 따라 변경
 }
 
 
 variable "availability_zone" {
   description = "Availability Zone to create the resources"
-  default     = "ap-northeast-2a" # 예시 AZ, 필요에 따라 변경
+  default     = "ap-northeast-2b" # 예시 AZ, 필요에 따라 변경
 }
